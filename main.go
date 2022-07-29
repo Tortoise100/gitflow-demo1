f6e0004803c8fa8b5f084f94fb6d9d2088c47a01
wangkang <wangkang@threegene.com>
2022-07-29 14:52:12 +0800

    fix bug

--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package main
 
 import "fmt"
 
-<<<<<<< HEAD
 func main() {
   fmt.Println("call main function")
-=======
-function main() {
-  fmt.Println("callmainfunction")
   fmt.Println("hello world")
->>>>>>> feature/print-hello-world
 }
